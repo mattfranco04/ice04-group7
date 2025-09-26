1399494b7a22a1f241e5823515fe7785553d8040
max-jeronimo <maxjeronimo05@gmail.com>
2025-09-26 16:01:47 -0400

    added Three.js example, made it so all three examples can be accessed through the console

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.sendFile(path.join(__dirname, "public", "jeronimo", "index.html"));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Three.js: http://localhost:${port}/jeronimo/`);
+    console.log(`D3:  http://localhost:${port}/franco/`);
+    console.log(`Tone.js: https://localhost:${port}/nemeti`)
 });
