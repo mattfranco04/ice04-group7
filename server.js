f94624dc676f25acd41953906dfccd12ea1abad2
Matthew Franco <matthew.m.franco@gmail.com>
2025-09-26 17:10:12 -0400

    fix routing for nemeti

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const path = require('path');
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "jeronimo", "index.html"));
@@ -15,7 +14,7 @@ app.get("/franco", (req, res) => {
 });
 
 app.get("/nemeti", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "nemeti", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "Nemeti", "index.html"));
 });
 
 app.listen(port, () => {
